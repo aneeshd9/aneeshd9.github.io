dbf52b55e1bd3c262fa7acc85f84cbbf5a4fc8eb
Aneesh Dandime <aneesh.dandime9@gmail.com>
2023-02-01 19:08:08 -0500

    Fixed navbar not collapsing.

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 // Bootstrap imports
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
